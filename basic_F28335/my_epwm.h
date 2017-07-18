7f9e577bf1d80761e6fb8c805a0b6ec20b893982
João Antônio Cardoso <joao.maker@gmail.com>
2017-07-18 03:38:27 -0300

    EPWM do Raul em arquivos separados (não testado)

--- basic_F28335/my_epwm.h
+++ basic_F28335/my_epwm.h
@@ -1,67 +1,21 @@
-/*
+/**
  * my_epwm.h
  *
- *  Created on: Jul 17, 2017
+ *  Created on: Jul 18, 2017
  *      Author: joaoantoniocardoso
  *
- *  Description:
- *      This example configures ePWM1, ePWM2, ePWM3 to produce an waveform with
- *      independent modulation on EPWMxA and EPWMxB. The compare values CMPA and
- *      CMPB are modified within the ePWM's ISR. The TB counter is in up/down
- *      count mode for this example.
+ *  Description: 
+ *
  */
+
+#include "DSP28x_Project.h"
 
 #ifndef MY_EPWM_H_
 #define MY_EPWM_H_
 
-#include "DSP28x_Project.h"
+void my_epwm_init(void);
 
-typedef struct
-{
-   volatile struct EPWM_REGS *EPwmRegHandle;
-   Uint16 EPwm_CMPA_Direction;
-   Uint16 EPwm_CMPB_Direction;
-   Uint16 EPwmTimerIntCount;
-   Uint16 EPwmMaxCMPA;
-   Uint16 EPwmMinCMPA;
-   Uint16 EPwmMaxCMPB;
-   Uint16 EPwmMinCMPB;
-}EPWM_INFO;
-
-void InitEPwm1Example(void);
-void InitEPwm2Example(void);
-void InitEPwm3Example(void);
-//__interrupt void epwm1_isr(void);
-//__interrupt void epwm2_isr(void);
-//__interrupt void epwm3_isr(void);
-void update_compare(EPWM_INFO*);
-
-// Global variables used in this example
-extern EPWM_INFO epwm1_info;
-extern EPWM_INFO epwm2_info;
-extern EPWM_INFO epwm3_info;
-
-// Configure the period for each timer
-#define EPWM1_TIMER_TBPRD  2000  // Period register
-#define EPWM1_MAX_CMPA     1950
-#define EPWM1_MIN_CMPA       50
-#define EPWM1_MAX_CMPB     1950
-#define EPWM1_MIN_CMPB       50
-
-#define EPWM2_TIMER_TBPRD  2000  // Period register
-#define EPWM2_MAX_CMPA     1950
-#define EPWM2_MIN_CMPA       50
-#define EPWM2_MAX_CMPB     1950
-#define EPWM2_MIN_CMPB       50
-
-#define EPWM3_TIMER_TBPRD  2000  // Period register
-#define EPWM3_MAX_CMPA      950
-#define EPWM3_MIN_CMPA       50
-#define EPWM3_MAX_CMPB     1950
-#define EPWM3_MIN_CMPB     1050
-
-// To keep track of which way the compare value is moving
-#define EPWM_CMP_UP   1
-#define EPWM_CMP_DOWN 0
+void InitEPwm1(void);
+void InitEPwm2(void);
 
 #endif /* MY_EPWM_H_ */
