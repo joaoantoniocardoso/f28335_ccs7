26ab6182626c5f530073750854ce2700bed3175a
João Antônio Cardoso <joao.maker@gmail.com>
2017-07-17 23:17:28 -0300

    ignored files cache was refreshed

--- basic_F28335/Debug_FLASH/subdir_rules.mk
+++ basic_F28335/Debug_FLASH/subdir_rules.mk
@@ -10,6 +10,13 @@ DSP2833x_ADC_cal.obj: /home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2
 	@echo 'Finished building: $<'
 	@echo ' '
 
+DSP2833x_Adc.obj: /home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_Adc.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"/opt/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 -O4 --opt_for_speed=5 --include_path="/opt/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/include" --include_path="/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_headers/include" --advice:performance=all -g --diag_suppress=10063 --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="DSP2833x_Adc.d" --verbose $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 DSP2833x_CodeStartBranch.obj: /home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_CodeStartBranch.asm $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: C2000 Compiler'
--- basic_F28335/Debug_FLASH/subdir_vars.mk
+++ basic_F28335/Debug_FLASH/subdir_vars.mk
@@ -13,6 +13,7 @@ ASM_SRCS += \
 /home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_usDelay.asm 
 
 C_SRCS += \
+/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_Adc.c \
 /home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_EPwm.c \
 /home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_headers/source/DSP2833x_GlobalVariableDefs.c \
 /home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_Gpio.c \
@@ -24,6 +25,7 @@ C_SRCS += \
 ../my_epwm.c 
 
 C_DEPS += \
+./DSP2833x_Adc.d \
 ./DSP2833x_EPwm.d \
 ./DSP2833x_GlobalVariableDefs.d \
 ./DSP2833x_Gpio.d \
@@ -36,6 +38,7 @@ C_DEPS += \
 
 OBJS += \
 ./DSP2833x_ADC_cal.obj \
+./DSP2833x_Adc.obj \
 ./DSP2833x_CodeStartBranch.obj \
 ./DSP2833x_EPwm.obj \
 ./DSP2833x_GlobalVariableDefs.obj \
@@ -55,6 +58,7 @@ ASM_DEPS += \
 
 OBJS__QUOTED += \
 "DSP2833x_ADC_cal.obj" \
+"DSP2833x_Adc.obj" \
 "DSP2833x_CodeStartBranch.obj" \
 "DSP2833x_EPwm.obj" \
 "DSP2833x_GlobalVariableDefs.obj" \
@@ -68,6 +72,7 @@ OBJS__QUOTED += \
 "my_epwm.obj" 
 
 C_DEPS__QUOTED += \
+"DSP2833x_Adc.d" \
 "DSP2833x_EPwm.d" \
 "DSP2833x_GlobalVariableDefs.d" \
 "DSP2833x_Gpio.d" \
@@ -83,11 +88,8 @@ ASM_DEPS__QUOTED += \
 "DSP2833x_CodeStartBranch.d" \
 "DSP2833x_usDelay.d" 
 
-ASM_SRCS__QUOTED += \
-"/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_CodeStartBranch.asm" \
-"/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_usDelay.asm" 
-
 C_SRCS__QUOTED += \
+"/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_Adc.c" \
 "/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_EPwm.c" \
 "/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_headers/source/DSP2833x_GlobalVariableDefs.c" \
 "/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_Gpio.c" \
@@ -97,5 +99,9 @@ C_SRCS__QUOTED += \
 "/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_Xintf.c" \
 "../main.c" \
 "../my_epwm.c" 
+
+ASM_SRCS__QUOTED += \
+"/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_CodeStartBranch.asm" \
+"/home/joaoantoniocardoso/workspace_CUKPFC/f2833x/v142/DSP2833x_common/source/DSP2833x_usDelay.asm" 
 
 
